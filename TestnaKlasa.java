df4c250ba0c3957344b6a961195ded4dd94a2aab
nedeljkoGrljac <46195075+nedeljkoGrljac@users.noreply.github.com>
2019-01-03 14:59:27 +0100

    Dodan ispis

--- TestnaKlasa.java
+++ TestnaKlasa.java
@@ -11,7 +11,7 @@ public class TestnaKlasa {
      * @param args
      */
     public static void main(String[] args) throws Exception{
-    	
+    	System.out.println("PROJECT TESTING");
     }
 }
 
