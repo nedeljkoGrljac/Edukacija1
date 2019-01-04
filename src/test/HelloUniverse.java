883e637c63a91b5c8921ac22a4a056d0da01d0f2
nedeljkoGrljac <46195075+nedeljkoGrljac@users.noreply.github.com>
2019-01-04 16:07:24 +0100

    HelloUniverse v0.2

--- src/test/HelloUniverse.java
+++ src/test/HelloUniverse.java
@@ -3,8 +3,15 @@ package test;
 public class HelloUniverse {
 
 	public static void main(String[] args) {
-		System.out.println("HelloUniverse");     
+		System.out.println("HelloUniverse");  
+		print(null);
+		print("HelloWorld not HelloUniverse");
 
+	}
+	
+	private static void print(Object o){
+		if(o==null) System.out.println("input object is null");  
+		else System.out.println("input object = " + o.toString());  
 	}
 
 }
